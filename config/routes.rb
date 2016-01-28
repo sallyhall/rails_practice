b712e6f7a568bf6d403ef67eafd87abed1956271
Sally Kingston <sally.a.kingston@gmail.com>
2016-01-27 22:10:58 -0500

    adding about, help, and home pages

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,10 @@ Rails.application.routes.draw do
 
   get 'static_pages/help'
 
+  get 'static_pages/about'
+
   # You can have the root of your site routed with "root"
   root 'application#hello'
 
-  
+
 end
